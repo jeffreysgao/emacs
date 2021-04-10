64615514171e85a5ae83142d827d9980f6cbb07c
Jeffrey Gao <jeffreygao@Jeffreys-MacBook-Pro-2.local>
2021-04-10 12:01:43 -0700

    Change home directory for init

--- platform.el
+++ platform.el
@@ -1,10 +1,10 @@
 (let ((home-dir (getenv "HOME")))
   (cond
    ;; ubuntu
-   ((equal home-dir "/home/john")
+   ((equal home-dir "/home/jeffreygao")
     (load-file "~/.emacs.d/platform/ubuntu.el"))
    ;; mac
-   ((equal home-dir "/Users/john")
+   ((equal home-dir "/Users/jeffreygao")
     (load-file "~/.emacs.d/platform/mac.el"))
    ;; windows...
    (t
